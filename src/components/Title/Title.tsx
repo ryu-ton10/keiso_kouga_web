1de50dee203d74c70fc11f9c7216552a5ff40371
RyuseiNomi <kanontonto1010@gmail.com>
2021-05-15 17:00:38 +0900

    [WIP]create title section

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Title.css';
+
+const Title: React.FC = () => {
+    return (
+        <div className={'title' + ' ' + 'title-main'}>
+            <div className="title-text">
+                <p>珪素光画</p>
+            </div>
+        </div>
+    );
+}
+
+export default Title;

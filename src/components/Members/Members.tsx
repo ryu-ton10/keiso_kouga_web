a98600756b9f2790355e819f2c6a53395c18d8d1
RyuseiNomi <kanontonto1010@gmail.com>
2021-05-16 18:33:17 +0900

    set background image for each section

--- src/components/Members/Members.tsx
+++ src/components/Members/Members.tsx
@@ -5,19 +5,23 @@ import { Row, Col } from 'antd';
 
 const Members: React.FC = () => {
     return (
-        <>
+        <div className="member">
             <div className="member-title">
                 メンバー
             </div>
             <div className="member-cell">
                 <Row gutter={12}>
-                    <Col lg={4} xs={8} className="member-icon"><img src={String(icon)} alt="icon" /></Col>
-                    <Col lg={8} xs={16} className="member-name"><p>とんと</p></Col>
-                    <Col lg={4} xs={8} className="member-icon"><img src={String(icon)} alt="icon" /></Col>
-                    <Col lg={8} xs={16} className="member-name"><p>つきー</p></Col>
+                    <Col lg={12} xs={24} className="member-icon">
+                        <img src={String(icon)} alt="icon" />
+                        <p>とんと</p>
+                    </Col>
+                    <Col lg={12} xs={24} className="member-icon">
+                        <img src={String(icon)} alt="icon" />
+                        <p>つきー</p>
+                    </Col>
                 </Row>
             </div>
-        </>
+        </div>
     );
 }
 
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,8 +3,11 @@ import '../../style.css';
 
 const Title: React.FC = () => {
     return (
-        <div className="title-text">
-            珪素光画
+        <div className="title">
+            <div className="title-text">
+                <h2>珪素光画</h2>
+                <h3>旅を紡ぐ、風景を切り取る、心に留まる風景は、一生の思い出になる。</h3>
+            </div>
         </div>
     );
 }

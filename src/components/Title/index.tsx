e522503b49301a000714a8b443bdf2ea8331df17
RyuseiNomi <kanontonto1010@gmail.com>
2021-05-17 17:48:52 +0900

    make fade animation to title texts

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -6,7 +6,7 @@ const Title: React.FC = () => {
         <div className="title">
             <div className="title-text">
                 <h2>珪素光画</h2>
-                <h3>旅を紡ぐ、風景を切り取る、心に留まる風景は、一生の思い出になる。</h3>
+                <h3>旅を、思い出を、紡ぐ。</h3>
             </div>
         </div>
     );

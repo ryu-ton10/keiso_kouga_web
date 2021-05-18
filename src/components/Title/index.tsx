34872bb1300dfffcbea6339b93152501237ad5cb
RyuseiNomi <kanontonto1010@gmail.com>
2021-05-18 18:51:23 +0900

    revise title section style

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -4,9 +4,11 @@ import '../../style.css';
 const Title: React.FC = () => {
     return (
         <div className="title">
-            <div className="title-text">
-                <h2>珪素光画</h2>
-                <h3>旅を、思い出を、紡ぐ。</h3>
+            <div className="title-rgba">
+                <div className="title-text">
+                    <div className="title-main">珪素光画</div>
+                    <div className="title-sub">旅を、思い出を、紡ぐ。</div>
+                </div>
             </div>
         </div>
     );

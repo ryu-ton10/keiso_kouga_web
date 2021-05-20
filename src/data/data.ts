504c2415ee8b5aff398c7e74242628acdd3cccff
RyuseiNomi <kanontonto1010@gmail.com>
2021-05-20 17:24:42 +0900

    replace profile icons extension to webp

--- src/data/data.ts
+++ src/data/data.ts
@@ -4,12 +4,12 @@ export const members = [
     {
         id: nanoid(),
         name: "とんと",
-        image: "tonto_icon.jpg",
+        image: "tonto_icon.webp",
     },
     {
         id: nanoid(),
         name: "つきうさぎ",
-        image: "tuki_icon.jpeg",
+        image: "tuki_icon.webp",
     },
 ]
 
@@ -18,6 +18,6 @@ export const publishments = [
         id: nanoid(),
         title: "紬 -つむぎ-",
         description: "日本各地の風景を集めた一冊。それらはまさに、日本の旅を紡いだ。",
-        image: "tsumugi.jpeg",
+        image: "tsumugi.webp",
     },
 ]

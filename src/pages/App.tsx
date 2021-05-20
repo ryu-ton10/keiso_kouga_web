f33e0a6c53200fcc537089683319f27f1e326306
RyuseiNomi <kanontonto1010@gmail.com>
2021-05-20 16:32:18 +0900

    add footer section

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,14 +6,21 @@ import Publishment from '../components/Publishment';
 import './App.css';
 import 'antd/dist/antd.css';
 import { members, publishments } from './../data/data';
+import { Layout } from 'antd';
 
 function App() {
+  
+  const { Footer } = Layout;
+
   return (
       <div className="main">
         <Title />
         <Description />
         <Members members={members}/>
         <Publishment publishments={publishments}/>
+        <Layout>
+          <Footer style={{ textAlign: 'center' }}>©︎ 2021 珪素光画</Footer>
+        </Layout>
       </div>
   );
 }

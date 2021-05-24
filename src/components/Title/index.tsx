15aa4318a2dae019d36e1d4d6d259971b94c65e0
RyuseiNomi <kanontonto1010@gmail.com>
2021-05-24 15:43:23 +0900

    apply media query for title section

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -6,8 +6,8 @@ const Title: React.FC = () => {
         <div className="title">
             <div className="title-rgba">
                 <div className="title-text">
-                    <div className="title-main">珪素光画</div>
-                    <div className="title-sub">日常を、思い出を、紡ぐ。</div>
+                    <p className="title-main">珪素光画</p>
+                    <p className="title-sub">日常を、思い出を、紡ぐ。</p>
                     <p className="scroll"><span></span></p>
                 </div>
             </div>

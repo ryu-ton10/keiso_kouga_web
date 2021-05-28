992dbf07720e0a50c0dfd465785c6790915e82a4
RyuseiNomi <kanontonto1010@gmail.com>
2021-05-28 12:41:34 +0900

    replace gallery images

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -8,11 +8,11 @@ const Gallery: React.FC = () => {
         <div className="gallery">
             <Row>
                 <Col xs={24} sm={24} md={12} lg={12} className="vertical">
-                    <Image src="Gallery/gallery_6.webp" />
+                    <Image src="Gallery/gallery_waterfall.webp" fallback="Gallery/gallery_waterfall.jpeg" />
                 </Col>
                 <Col xs={24} sm={24} md={12} lg={12} className="horizonal">
-                    <Image src="Gallery/gallery_5.webp" />
-                    <Image src="Gallery/gallery_2.webp" />
+                    <Image src="Gallery/gallery_mountain.webp" fallback="Gallery/gallery_mountain.jpeg" />
+                    <Image src="Gallery/gallery_susukino.webp" fallback="Gallery/gallery_susukino.jpg" />
                 </Col>
             </Row>
         </div>

998078cf5509499f5a73b7d7575b13fefdcc59a3
RyuseiNomi <kanontonto1010@gmail.com>
2021-06-17 21:19:01 +0900

    set prev image for gallery and publishment

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -8,11 +8,29 @@ function Gallery() {
         <div className="gallery">
             <Row>
                 <Col xs={24} sm={24} md={12} lg={12} className="vertical">
-                    <Image src="Gallery/gallery_waterfall.webp" fallback="Gallery/gallery_waterfall.jpeg" />
+                    <Image 
+                        src="Gallery/gallery_waterfall_prev.webp"
+                        fallback="Gallery/gallery_waterfall_prev.jpeg"
+                        preview={{
+                            src: "Gallery/gallery_waterfall.webp",
+                        }}
+                    />
                 </Col>
                 <Col xs={24} sm={24} md={12} lg={12} className="horizonal">
-                    <Image src="Gallery/gallery_mountain.webp" fallback="Gallery/gallery_mountain.jpeg" />
-                    <Image src="Gallery/gallery_susukino.webp" fallback="Gallery/gallery_susukino.jpg" />
+                    <Image
+                        src="Gallery/gallery_mountain_prev.webp"
+                        fallback="Gallery/gallery_mountain_prev.jpeg"
+                        preview={{
+                            src: "Gallery/gallery_mountain.webp",
+                        }}
+                    />
+                    <Image
+                        src="Gallery/gallery_susukino_prev.webp"
+                        fallback="Gallery/gallery_susukino_prev.jpg"
+                        preview={{
+                            src: "Gallery/gallery_susukino.webp",
+                        }}
+                    />
                 </Col>
             </Row>
         </div>
--- src/data/data.ts
+++ src/data/data.ts
@@ -134,7 +134,9 @@ export const books = [
         description: "珪素光画の第1作目として刊行された作品。日本各地の風景を撮影したものを集め、まるで1つの旅をしているかのように紡いだ１冊です。",
         image: {
             webp: "Publishments/tsumugi.webp",
-            jpeg: "Publishments/tsumugi.jpeg"
+            jpeg: "Publishments/tsumugi.jpeg",
+            pre_webp: "Publishments/tsumugi_prev.webp",
+            pre_jpeg: "Publishments/tsumugi_prev.jpeg",
         },
         url: "https://www.amazon.co.jp/dp/B08RY6VBMP/ref=cm_sw_r_tw_awdb_c_x_GxP8FbQ43252P",
     },

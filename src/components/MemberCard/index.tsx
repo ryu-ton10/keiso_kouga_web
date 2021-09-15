3da9b90f558d7f17601623465ba1c3d78b769cf2
RyuseiNomi <kanontonto1010@gmail.com>
2021-09-15 22:01:04 +0900

    revise member and member-card css

--- src/components/MemberCard/index.tsx
+++ src/components/MemberCard/index.tsx
@@ -33,39 +33,41 @@ SwiperCore.use([Pagination, Autoplay]);
 function MemberCard(props: Props) {
 
     return (
-        <Row>
-            <Col xs={24} sm={8} md={8} lg={8} className="member-profile-left">
-                <picture>
-                    <source type="image/webp" srcSet={process.env.PUBLIC_URL + props.icon.webp}></source>
-                    <img src={process.env.PUBLIC_URL + props.icon.jpeg} alt="icon" />
-                </picture>
-                <Row className="names">
-                    <p className="name">{props.name}</p>
-                    <a href={props.twitter_url} rel="noopener noreferrer" target="_blank"><TwitterOutlined /></a>
-                </Row>
-                <p>使用機材: {props.camera}</p>
-            </Col>
-            <Col xs={24} sm={16} md={16} lg={16} className="member-profile-right">
-                <Swiper
-                    autoplay={{delay: 2000}}
-                    pagination={{ clickable: true }}
-                >
-                    {props.works.map((work) => {
-                        return (
-                            <SwiperSlide>
-                                <Image
-                                    src={process.env.PUBLIC_URL + work.pre_webp}
-                                    fallback={process.env.PUBLIC_URL + work.pre_jpeg}
-                                    preview={{
-                                        src: `${process.env.PUBLIC_URL + work.webp}`,
-                                    }}
-                                />
-                            </SwiperSlide>
-                        );
-                    })}
-                </Swiper>
-            </Col>
-        </Row>
+        <div className="member-card">
+            <Row>
+                <Col xs={24} sm={8} md={8} lg={8} className="member-profile-left">
+                    <picture>
+                        <source type="image/webp" srcSet={process.env.PUBLIC_URL + props.icon.webp}></source>
+                        <img src={process.env.PUBLIC_URL + props.icon.jpeg} alt="icon" />
+                    </picture>
+                    <Row className="names">
+                        <p className="name">{props.name}</p>
+                        <a href={props.twitter_url} rel="noopener noreferrer" target="_blank"><TwitterOutlined /></a>
+                    </Row>
+                    <p>使用機材: {props.camera}</p>
+                </Col>
+                <Col xs={24} sm={16} md={16} lg={16} className="member-profile-right">
+                    <Swiper
+                        autoplay={{delay: 2000}}
+                        pagination={{ clickable: true }}
+                    >
+                        {props.works.map((work) => {
+                            return (
+                                <SwiperSlide>
+                                    <Image
+                                        src={process.env.PUBLIC_URL + work.pre_webp}
+                                        fallback={process.env.PUBLIC_URL + work.pre_jpeg}
+                                        preview={{
+                                            src: `${process.env.PUBLIC_URL + work.webp}`,
+                                        }}
+                                    />
+                                </SwiperSlide>
+                            );
+                        })}
+                    </Swiper>
+                </Col>
+            </Row>
+        </div>
     );
 }
 

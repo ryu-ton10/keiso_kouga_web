c22c0ea0f638b34524912d5bf33ff0a96c7cc0f8
RyuseiNomi <kanontonto1010@gmail.com>
2021-11-20 22:29:24 +0900

    replace logo image

--- src/pages/Description/index.tsx
+++ src/pages/Description/index.tsx
@@ -6,8 +6,8 @@ function Description() {
     <div className="description">
       <div className="logo">
         <picture>
-          <source type="image/webp" srcSet={process.env.PUBLIC_URL + 'keiso_logo.webp'}></source>
-          <img src={process.env.PUBLIC_URL + 'keiso_logo.png'} alt="icon" />
+          <source type="image/webp" srcSet={process.env.PUBLIC_URL + 'keiso_kouga_logo.webp'}></source>
+          <img src={process.env.PUBLIC_URL + 'keiso_kouga_logo.png'} alt="icon" />
         </picture>
       </div>
       <div className="description-title">

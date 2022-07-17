270afbdd7ef0fb1a5d1b153677aa4551b1c5ea25
RyuseiNomi <kanontonto1010@gmail.com>
2022-07-17 17:36:05 +0900

    install grid-layout for Gallery component

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -1,41 +1,37 @@
 import React from 'react';
 import './index.css';
-import { Row, Col, Image } from 'antd';
+import { Image } from 'antd';
 
 function Gallery() {
 
   return (
     <div className="gallery">
-      <Row>
-        <Col xs={24} sm={24} md={12} lg={12} className="vertical">
-          <Image
-            src="Gallery/gallery_waterfall_prev.webp"
-            fallback="Gallery/gallery_waterfall_prev.jpeg"
-            alt="赤目四十八滝"
-            preview={{
-                src: "Gallery/gallery_waterfall.webp",
-            }}
-          />
-        </Col>
-        <Col xs={24} sm={24} md={12} lg={12} className="horizonal">
-          <Image
-            src="Gallery/gallery_mountain_prev.webp"
-            fallback="Gallery/gallery_mountain_prev.jpeg"
-            alt="見通しの良い道"
-            preview={{
-                src: "Gallery/gallery_mountain.webp",
-            }}
-          />
-          <Image
-            src="Gallery/gallery_susukino_prev.webp"
-            fallback="Gallery/gallery_susukino_prev.jpg"
-            alt="北海道すすきの"
-            preview={{
-                src: "Gallery/gallery_susukino.webp",
-            }}
-          />
-        </Col>
-      </Row>
+      <Image
+        src="Gallery/gallery_waterfall_prev.webp"
+        fallback="Gallery/gallery_waterfall_prev.jpeg"
+        alt="赤目四十八滝"
+        preview={{
+            src: "Gallery/gallery_waterfall.webp",
+        }}
+      />
+      <div className="gallery-right">
+        <Image
+          src="Gallery/gallery_mountain_prev.webp"
+          fallback="Gallery/gallery_mountain_prev.jpeg"
+          alt="見通しの良い道"
+          preview={{
+              src: "Gallery/gallery_mountain.webp",
+          }}
+        />
+        <Image
+          src="Gallery/gallery_susukino_prev.webp"
+          fallback="Gallery/gallery_susukino_prev.jpg"
+          alt="北海道すすきの"
+          preview={{
+              src: "Gallery/gallery_susukino.webp",
+          }}
+        />
+      </div>
     </div>
   );
 }
